72b49e8c227c9ea6b13d4225f8e703095343c08d
G0erman <german.drf@gmail.com>
2021-01-09 09:11:59 +0000

    Fix give feedback for the wrong first answer

--- guessinggame.sh
+++ guessinggame.sh
@@ -2,10 +2,11 @@
 # File: guessinggame.sh
 
 echo "How many files there are in the current directory?"
-read response
+
 total_files=$(ls|wc -l)
 
 function guess {
+	# Compare user response with calculate files
 	if [[ $1 -eq $2 ]]
 	then
 		echo "you_rock"
@@ -17,20 +18,24 @@ function guess {
 	fi
 }
 
-result=$(guess response total_files)
+function decode_msg {
+	# Interprete guess result
+        read response
+        result=$(guess response total_files)
+        if [[ $result -eq 1000 ]]
+        then
+                echo "Your answer is too high."
+        elif [[ $result -eq 1 ]]
+        then
+                echo "Your answer is too low."
+	fi
+}
 
+decode_msg
 until [ $result = "you_rock" ]
 do
 	echo "Please try again: "
-	read response
-	result=$(guess response total_files)
-	if [[ $result -eq 1000 ]]
-	then
-		echo "Your answer is too high"
-	elif [[ $result -eq 1 ]]
-	then
-		echo "Your answer is too low"
-	fi
+	decode_msg
 done
 
 echo "congratulations you rock!"
